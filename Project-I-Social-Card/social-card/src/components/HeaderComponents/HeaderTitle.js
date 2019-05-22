fa1a8f70082615e87932601b21c81c30a98a99d8
Donald Whitely <dswhitely1@gmail.com>
2019-05-22 12:13:54 -0400

    Header Components Built

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+	return (
+		<div>
+			<p>
+				Lambda School<span>@LambdaSchool 26 jan</span>
+			</p>
+		</div>
+	);
+};
+
+export default HeaderTitle;
